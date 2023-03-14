8f9cbc5d7c362c533693b62be9128a984abcb803
Hpeng99 <122468233+Hpeng99@users.noreply.github.com>
2023-03-13 21:10:47 -0400

    fasta files
    output.fast    bats_ace2.1.fasta  is question 1
    
    rest is question 2

---
+++ hw5.sh
@@ -0,0 +1,5 @@
+muscle -in bats_ace2.fasta -out bats_finished.fasta
+raxmlHPC-PTHREADS-AVX -t <3> -s bats_ace2_aligned.fasta -n <bats_ace2_tree> -m <S7E> -p <15> -b <1> -f <RAxML_bootstrap.T14
+#I hope these are right ; - ; 
+#4
+#5 This can be used as something that you can use to find the source of the the virus or you can use something that is similar to the animal on the tree to see how they deal with the virus then the virus is introduced to them
